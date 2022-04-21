9e280dd8da57093ff36188f8fffc404e7ffb92e8
Omar Costilla <omarcostilla6@gmail.com>
2022-04-21 10:56:51 -0700

    Demo 4 finished

--- app/src/main/java/com/example/scheduleviewer/MainActivity.kt
+++ app/src/main/java/com/example/scheduleviewer/MainActivity.kt
@@ -5,131 +5,51 @@ import android.os.Bundle
 import android.widget.Switch
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.reflect.Type
 import java.time.LocalDateTime
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    val events = listOf(
-        Event(
-            "ECS 198F Android App Dev",
-            "Lecture",
-            LocalDateTime.of(2022, 4, 4, 16, 10),
-            LocalDateTime.of(2022, 4, 4, 17, 0),
-            "Wellman Hall 212"
-        ),
-        Event(
-            "ECS 198F Android App Dev",
-            "Lecture",
-            LocalDateTime.of(2022, 4, 6, 16, 10),
-            LocalDateTime.of(2022, 4, 6, 17, 0),
-            "Wellman Hall 212"
-        ),
-        Event(
-            "ECS 198F Android App Dev",
-            "Office Hour",
-            LocalDateTime.of(2022, 4, 5, 10, 30),
-            LocalDateTime.of(2022, 4, 5, 11, 45),
-            "Zoom"
-        ),
-        Event(
-            "ECS 198F Android App Dev",
-            "Office Hour",
-            LocalDateTime.of(2022, 4, 7, 10, 30),
-            LocalDateTime.of(2022, 4, 7, 11, 45),
-            "Zoom"
-        ),
-        Event(
-            "ECS 122B Algorithm Design and Analysis",
-            "Lecture",
-            LocalDateTime.of(2022, 4, 4, 11, 0),
-            LocalDateTime.of(2022, 4, 4, 11, 50),
-            "Veihmeyer Hall 212"
-        ),
-        Event(
-            "ECS 122B Algorithm Design and Analysis",
-            "Lecture",
-            LocalDateTime.of(2022, 4, 6, 11, 0),
-            LocalDateTime.of(2022, 4, 6, 11, 50),
-            "Veihmeyer Hall 212"
-        ),
-        Event(
-            "ECS 122B Algorithm Design and Analysis",
-            "Lecture",
-            LocalDateTime.of(2022, 4, 8, 11, 0),
-            LocalDateTime.of(2022, 4, 8, 11, 50),
-            "Veihmeyer Hall 212"
-        ),
-        Event(
-            "ECS 122B Algorithm Design and Analysis",
-            "Office Hour",
-            LocalDateTime.of(2022, 4, 5, 8, 0),
-            LocalDateTime.of(2022, 4, 5, 9, 0),
-            "Zoom"
-        ),
-        Event(
-            "ECS 122B Algorithm Design and Analysis",
-            "Office Hour",
-            LocalDateTime.of(2022, 4, 7, 9, 0),
-            LocalDateTime.of(2022, 4, 7, 10, 0),
-            "Zoom"
-        ),
-        Event(
-            "ECS 162 Web Programming",
-            "Lecture",
-            LocalDateTime.of(2022, 4, 4, 9, 0),
-            LocalDateTime.of(2022, 4, 4, 9, 50),
-            "Social Sciences & Humanities 1100"
-        ),
-        Event(
-            "ECS 162 Web Programming",
-            "Lecture",
-            LocalDateTime.of(2022, 4, 6, 9, 0),
-            LocalDateTime.of(2022, 4, 6, 9, 50),
-            "Social Sciences & Humanities 1100"
-        ),
-        Event(
-            "ECS 162 Web Programming",
-            "Lecture",
-            LocalDateTime.of(2022, 4, 8, 9, 0),
-            LocalDateTime.of(2022, 4, 8, 9, 50),
-            "Social Sciences & Humanities 1100"
-        ),
-        Event(
-            "ECS 162 Web Programming",
-            "Office Hour",
-            LocalDateTime.of(2022, 4, 4, 11, 0),
-            LocalDateTime.of(2022, 4, 4, 12, 0),
-            "Zoom"
-        ),
-        Event(
-            "ECS 152A Computer Networks",
-            "Lecture",
-            LocalDateTime.of(2022, 4, 4, 14, 10),
-            LocalDateTime.of(2022, 4, 4, 15, 0),
-            "Giedt 1003"
-        ),
-        Event(
-            "ECS 152A Computer Networks",
-            "Lecture",
-            LocalDateTime.of(2022, 4, 6, 14, 10),
-            LocalDateTime.of(2022, 4, 6, 15, 0),
-            "Giedt 1003"
-        ),
-        Event(
-            "ECS 152A Computer Networks",
-            "Lecture",
-            LocalDateTime.of(2022, 4, 8, 14, 10),
-            LocalDateTime.of(2022, 4, 8, 15, 0),
-            "Giedt 1003"
-        ),
-        Event(
-            "ECS 152A Computer Networks",
-            "Office Hour",
-            LocalDateTime.of(2022, 4, 4, 13, 0),
-            LocalDateTime.of(2022, 4, 4, 14, 0),
-            "Kemper 3043"
-        ),
-    ).sortedBy { it.from }
+    // create custom Gson instance to use LocalDateTime class
+    val gson: Gson = GsonBuilder()
+        .registerTypeAdapter(LocalDateTime::class.java, object : JsonDeserializer<LocalDateTime> {
+            // need deserialize for retrieving data
+            override fun deserialize(
+                json: JsonElement?,
+                typeOfT: Type?,
+                context: JsonDeserializationContext?
+            ): LocalDateTime {
+                return LocalDateTime.parse(json!!.asString)
+            }
+        })
+        .registerTypeAdapter(LocalDateTime::class.java, object : JsonSerializer<LocalDateTime> {
+            override fun serialize(
+                src: LocalDateTime?,
+                typeOfSrc: Type?,
+                context: JsonSerializationContext?
+            ): JsonElement {
+               return JsonPrimitive(src!!.toString())
+            }
+        })
+        .create()
+
+
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.scheduledemo.schedgo.com")
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+        .create(ScheduleService::class.java) // pass in interface of requests
+
+    val events = listOf<Event>()
+
+    val adapter = EventListRecyclerViewAdapter(events)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -139,7 +59,7 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<RecyclerView>(R.id.recyclerView).apply {
 
-            this.adapter = adapter
+            this.adapter = this@MainActivity.adapter
             layoutManager = LinearLayoutManager(context)
         }
 
@@ -147,13 +67,24 @@ class MainActivity : AppCompatActivity() {
         findViewById<Switch>(R.id.officeHoursSwitch).apply {
             isChecked = true
             setOnCheckedChangeListener { _, checked ->
-                adapter.updateEvents(when (checked) {
-                    true -> events
-                    false -> events.filter { it.type != "Office Hour" }
-                })
+                updateList(checked)
             }
-
         }
 
+        updateList(true)
+    }
+
+    private fun updateList(includesOfficeHours: Boolean) {
+        service.listEvents(includesOfficeHours).enqueue(object : Callback<List<Event>> {
+            override fun onResponse(call: Call<List<Event>>, response: Response<List<Event>>) {
+                // update content in recyclerView with events in response
+                adapter.updateEvents(response.body()!!)
+            }
+
+            override fun onFailure(call: Call<List<Event>>, t: Throwable) {
+                throw t
+            }
+
+        })
     }
 }
--- app/src/main/java/com/example/scheduleviewer/ScheduleService.kt
+++ app/src/main/java/com/example/scheduleviewer/ScheduleService.kt
@@ -1,11 +1,24 @@
 package com.example.scheduleviewer
 
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface ScheduleService {
     // Define endpoints
+
+    // GET request collects all the taco truck Events
     @GET("/events")
     fun listEvents(@Query("includesOfficeHours") includesOfficeHours: Boolean): Call<List<Event>>
+
+    // POST response has no body, Unit is the kotlin way of saying void type
+    @POST("/events")
+    fun createEvent(@Body event: Event): Call<Unit>
+
+    // GET request retrieves details of one truck
+    @GET("/events/{id}")
+    fun getEvent(@Path("id") id: Int): Call<Event>
+
+    // DELETE an item
+    @GET("events/{id}")
+    fun deleteEvent(@Path("id") id: Int): Call<Unit>
 }
